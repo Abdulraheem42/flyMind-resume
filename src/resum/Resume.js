bd2be4bc944097ed590246452fc6e1acb93668e9
Abdul Raheem <raheem.scorpio@gmail.com>
2020-05-12 12:50:40 +0500

    Fixed responsiveness resume

--- src/resum/Resume.js
+++ src/resum/Resume.js
@@ -23,70 +23,78 @@ class Resume extends Component {
                     <Navbar fullName={fullName} targetRef={ref} />
                 </div>
             <div className='container d-flex justify-content-center flex-column'>
-                <div ref={ref} className='resume-wrapper'>
-                <div className='d-flex align-items-center'>
-                    <span className=''><img src={logo} width='150px' alt=""/></span>
-                    <span className='font-size45 ml-5'>
-                        {fullName}<br/>
-                        <h4 className='theme-color'>{jobTitle}</h4>
-                    </span>
-                </div>
-                <div className='mt-2'>
-                    <h1 className='theme-color underline'>Personal Details:</h1>
-                    <div>
-                    <div><span className='font-size15'>Address: </span> {address}</div>
-                    <div><span className='font-size15'>Email: </span> {email}</div>
-                    <div><span className='font-size15'>Phone#: </span> {phone}</div>
-                    </div>
-                    </div>
-                    <div className=''>
-                        {/*<div><span className='font-size15'>Father's Name:</span> {fatherName}</div>*/}
-                        <div><span className='font-size15'>Gender:</span> {gender}</div>
-                        <div><span className='font-size15'>Nationality:</span> {country}</div>
-                        <div><span className='font-size15'>Region:</span>{region}</div>
-                    </div>
+                <div className='row resume-wrapper'>
+                    <div className='col-lg-10'>
+                        <div ref={ref} className=''>
+                            <div className='row d-flex align-items-center'>
+                                <span className=''>
+                                    <img src={logo} width='150px' alt=""/>
+                                </span>
+                                <span className='font-size45'>
+                                    {fullName}<br/>
+                                    <h4 className='theme-color'>{jobTitle}</h4>
+                                </span>
+                            </div>
+                            <div className='mt-2'>
+                                <h1 className='theme-color underline'>Personal Details:</h1>
+                                <div>
+                                    <div><span className='font-size15'>Address: </span> {address}</div>
+                                    <div><span className='font-size15'>Email: </span> {email}</div>
+                                    <div><span className='font-size15'>Phone#: </span> {phone}</div>
+                                </div>
+                            </div>
+                            <div className=''>
+                                {/*<div><span className='font-size15'>Father's Name:</span> {fatherName}</div>*/}
+                                <div><span className='font-size15'>Gender:</span> {gender}</div>
+                                <div><span className='font-size15'>Nationality:</span> {country}</div>
+                                <div><span className='font-size15'>Region:</span>{region}</div>
+                            </div>
 
-                    <div className='mt-2'>
-                        <h1 className='theme-color underline'>Summary:</h1>
-                        <li className='font-size15'>{describeEdu}</li>
-                    </div>
-                    <div className='mt-4'>
-                        <h1 className='theme-color underline'>Work Experience:</h1>
-                        <div><span className='font-size20'>Company Name: </span> {companyName}</div>
-                        <div><span className='font-size20'>Duration: </span> {startyear} - {endyear}</div>
-                    </div>
-                    <div className='mt-4'>
-                        <h1 className='theme-color underline'>Education:</h1>
-                        <div>
+                            <div className='mt-2'>
+                                <h1 className='theme-color underline'>Summary:</h1>
+                                <li className='font-size15'>{describeEdu}</li>
+                            </div>
+                            <div className='mt-4'>
+                                <h1 className='theme-color underline'>Work Experience:</h1>
+                                <div><span className='font-size20'>Company Name: </span> {companyName}</div>
+                                <div><span className='font-size20'>Duration: </span> {startyear} - {endyear}</div>
+                            </div>
+                            <div className='mt-4'>
+                                <h1 className='theme-color underline'>Education:</h1>
+                                <div>
                             <span className='font-size20'>School Name:
                                 <h4 className='theme-color'>{schoolName} -{graduationDate}</h4>
                             </span>
-                            <div><span className='font-size15'>City:</span>{city}</div>
-                            <div><span className='font-size15'>Degree:</span>{degree}</div>
+                                    <div><span className='font-size15'>City:</span>{city}</div>
+                                    <div><span className='font-size15'>Degree:</span>{degree}</div>
+                                </div>
+                            </div>
+
+                            <div className=''>
+                                <div className='mt-4'>
+                                    <h1 className='theme-color underline'>Skills:</h1>
+                                    <ul>
+                                        {selectedOption ? selectedOption.map( select => {
+                                            return(
+                                                <li key={select.id}>{select.value}</li>
+                                            )
+                                        }) : null}
+                                    </ul>
+                                </div>
+
+                                <div className='mt-4'>
+                                    <h1 className='theme-color underline'>Language:</h1>
+                                    {language ? language.map(res =>{
+                                        return(
+                                            <span key={res.id}>{res.value}, </span>
+                                        )
+                                    }) : null}
+                                </div>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className=''>
-                    <div className='mt-4'>
-                        <h1 className='theme-color underline'>Skills:</h1>
-                        <ul>
-                            {selectedOption ? selectedOption.map( select => {
-                                return(
-                                    <li key={select.id}>{select.value}</li>
-                                )
-                            }) : null}
-                        </ul>
                     </div>
-
-                    <div className='mt-4'>
-                        <h1 className='theme-color underline'>Language:</h1>
-                        {language ? language.map(res =>{
-                            return(
-                                    <span key={res.id}>{res.value}, </span>
-                            )
-                        }) : null}
-                    </div>
-                </div>
+                    <div className='col-lg-2'></div>
                 </div>
             </div>
             </div>
